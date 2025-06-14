b1a6100ad98230a4dc801340b1b7598bf5b25384
TinTanSan <tirthvpatel2003@gmail.com>
2025-06-14 21:42:52 +1000

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Home() {
+
+
+  return (
+    <div className='flex flex-col w-screen h-screen'>
+      <nav className='h-15 flex w-full justify-center'>Finance game</nav>
+      <main className='flex flex-col'>
+        
+      </main>
+    </div>
+  )
+}
