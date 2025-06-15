0b51dbfac97349bc3715ac8a3007db92d731d46c
TinTanSan <tirthvpatel2003@gmail.com>
2025-06-15 14:42:50 +1000

    added padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
 
 
   return (
-    <div className='flex flex-col w-screen h-screen'>
+    <div className='flex flex-col w-screen h-screen p-1'>
       <nav className='h-15 flex w-full justify-center'>Finance game</nav>
       <main className='flex flex-col'>
         Hello      
