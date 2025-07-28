79e587324b51440d7d1d5a523f4548fcbb0e7c8d
TinTanSan <tirthvpatel2003@gmail.com>
2025-07-29 07:00:32 +1000

    added a game state context to keep track of the gamestate

---
+++ src/app/contexts/gameStateContext.tsx
@@ -0,0 +1,23 @@
+import { Deposit } from "@/interfaces/deposit";
+import { Property } from "@/interfaces/property";
+import { Holding, Security } from "@/interfaces/securities";
+import { createContext } from "react";
+
+
+export interface GameState{
+    month:number,
+    balance:number,
+    
+    securities: Array<Security>,
+    holdings: Array<Holding>,
+
+    deposits: Array<Deposit>,
+    properties: Array<Property>
+}
+
+interface GameStateCtxType{
+    gameState: GameState | undefined,
+    setGameState: React.Dispatch<React.SetStateAction<GameState | undefined>>
+}
+
+export const GameStateContext = createContext<GameStateCtxType | undefined>(undefined)
