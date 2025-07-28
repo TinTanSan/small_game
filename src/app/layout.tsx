461776987ca1c5d35755b2549695f23182d782d8
TinTanSan <tirthvpatel2003@gmail.com>
2025-07-29 06:59:30 +1000

    experimenting with the usage of contexts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,31 @@
+'use client'
+import { useEffect, useState } from "react";
 import "./globals.css";
+import { GameState, GameStateContext } from "./contexts/gameStateContext";
+import { importFromLocalStorage, setUpLocalStorage } from "@/utils/commons";
+import Navbar from "./components/navbar";
+import { PageContext } from "./contexts/pageContext";
 
 export default function RootLayout({children,}: Readonly<{children: React.ReactNode;}>) {
+  const [gameState, setGameState] = useState<GameState | undefined>(undefined);
+  const [page, setPage] = useState("Home");
+  useEffect(()=>{
+    setUpLocalStorage() 
+    setGameState(importFromLocalStorage())
+  },[])
   return (
     <html lang="en">
-      <body>
-        {children}
-      </body>
+      <GameStateContext.Provider value={{gameState, setGameState}}>
+        <PageContext.Provider value={{page, setPage}}>
+          
+            
+          <body>
+            <Navbar />
+            {children}
+          </body>
+        </PageContext.Provider>
+      </GameStateContext.Provider>
+      
     </html>
   );
 }
