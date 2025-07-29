05c7c2893aedc9cd0aa281e30b36d58c1539f72e
TinTanSan <tirthvpatel2003@gmail.com>
2025-07-29 10:16:04 +1000

    removed log statements and added the page name

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,25 +1,25 @@
 import React, { useContext, useState } from 'react'
 import { GameStateContext } from '../contexts/gameStateContext'
 import { updateGameState } from '@/utils/commons';
+import { PageContext } from '../contexts/pageContext';
 
 export default function Navbar() {
   const gameContext = useContext(GameStateContext);
+  const pageContext = useContext(PageContext);
   const [update,setUpdate] = useState(false);
   const handleNextMonth = ()=>{
-    console.log("cli  ")
     if (gameContext!== undefined && gameContext.gameState !== undefined){
-      console.log("beginning update")
       gameContext.setGameState(updateGameState(gameContext.gameState, 100))
-      console.log('update comeplete')
       setUpdate(!update)
     }
     
   }
   return (
     <nav className='flex w-full h-14 items-center px-5 justify-between'>
-        <div className="text-2xl">${gameContext?.gameState?.balance.toFixed(2)}</div>
-        <div>Month: {gameContext?.gameState?.month}</div>
-        <button onClick={()=>{handleNextMonth()}} className='flex bg-secondary text-secondary-content hover:bg-secondary-darken items-center justify-center w-32 h-8 rounded-lg '>Next Month</button>
+        <div className="text-xl flex w-1/2">${gameContext?.gameState?.balance.toFixed(2)}</div>
+        <div className='flex w-1/2 text-lg'>Month: {gameContext?.gameState?.month}</div>
+        <div className='flex w-full text-2xl'>{pageContext?.page || "Home"}</div>
+        <button onClick={()=>{handleNextMonth()}} className='flex bg-secondary text-secondary-content hover:bg-secondary-darken transition-colors duration-700 items-center justify-center w-fit flex-nowrap text-nowrap px-2 h-8 rounded-lg '>Next Month</button>
     </nav>
   )
 }
