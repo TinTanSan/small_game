bf76afcae718cb844ff6265ad34d910a9d1e1fd6
TinTanSan <tirthvpatel2003@gmail.com>
2026-01-16 09:33:57 +1100

    added styling to body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,16 @@
 'use client'
-import { useEffect, useState } from "react";
+import { isValidElement, ReactElement, ReactNode, useEffect, useState } from "react";
 import "./globals.css";
 import { GameState, GameStateContext } from "./contexts/gameStateContext";
 import { importFromLocalStorage, setUpLocalStorage } from "@/utils/commons";
 import Navbar from "./components/navbar";
 import { PageContext } from "./contexts/pageContext";
+import { headers } from "next/headers";
 
 export default function RootLayout({children,}: Readonly<{children: React.ReactNode;}>) {
   const [gameState, setGameState] = useState<GameState | undefined>(undefined);
   const [page, setPage] = useState("Home");
+
   useEffect(()=>{
     setUpLocalStorage() 
     setGameState(importFromLocalStorage())
@@ -16,10 +18,8 @@ export default function RootLayout({children,}: Readonly<{children: React.ReactN
   return (
     <html lang="en">
       <GameStateContext.Provider value={{gameState, setGameState}}>
-        <PageContext.Provider value={{page, setPage}}>
-          
-            
-          <body>
+        <PageContext.Provider value={{page, setPage}}>  
+          <body className="flex flex-col w-screen h-screen p-2 overflow-hidden bg-slate-500">
             <Navbar />
             {children}
           </body>
