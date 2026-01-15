01b61615def4285c975b2e91aa16a90c64020338
TinTanSan <tirthvpatel2003@gmail.com>
2026-01-16 10:38:47 +1100

    added colour coding based on change in previous month, such that if the change was positive the graph is green and if change was negative then graph is red.

--- src/app/securities/components/MiniGraph.tsx
+++ src/app/securities/components/MiniGraph.tsx
@@ -8,11 +8,16 @@ import { GameStateContext } from '@/app/contexts/gameStateContext';
 export default function MiniGraph({security}:{security:Security}) {
     const gameState = useContext(GameStateContext);
     if(!gameState || !gameState.gameState) throw new Error("Cannot display miniGraph without game state being defined, encountered an undefined version");
+    const previousClose = security.priceHistory.at(-2) ?? 0;
+    const currentClose = security.priceHistory.at(-1) ?? 0;
+    const changeSinceLastMonth = (currentClose-previousClose);
+    const percentageChange = (changeSinceLastMonth/currentClose)*100;
+    const graphColor = changeSinceLastMonth > 0? "oklch(52.7% 0.154 150.069)" : changeSinceLastMonth=== 0 ? "oklch(48.8% 0.243 264.376)":"oklch(50.5% 0.213 27.518)" ;
     return (
-        <div className='flex flex-row w-full h-fit items-center justify-between2 shadow-lg border-2 border-zinc-300 rounded-xl gap-5 text-lg p-2'>
-            <div className='flex text-lg w-fit px-2 text-nowrap'>{security.name}</div>
+        <div className='flex flex-row w-full h-fit items-center justify-between2 shadow-lg/20 border-2 border-zinc-300 rounded-xl bg-base-100 gap-5 text-lg p-2'>
+            <div className='flex text-lg w-1/4 px-2 text-nowrap'>{security.name}</div>
             {/* mini line graph */}
-            <div className='flex w-fit h-fit border-2 border-dashed border-blue-200 rounded-lg'>
+            <div className='flex w-fit h-fit border-2    border-dashed border-blue-200 rounded-lg'>
             <LineChart 
             margin={{
                 left:0,
@@ -21,7 +26,7 @@ export default function MiniGraph({security}:{security:Security}) {
                 bottom:0
             }}
                 sx={{
-                maxHeight:'5rem',
+                maxHeight:'4rem',
                 maxWidth: '15rem'
                 }}
                 yAxis={[{
@@ -40,7 +45,7 @@ export default function MiniGraph({security}:{security:Security}) {
                 data: security.priceHistory,
                 area:true,
                 showMark:false,
-                color:'oklch(0.5608 0.1653 255.71)',
+                color:graphColor,
                 }]}
             />
             </div>
@@ -48,10 +53,10 @@ export default function MiniGraph({security}:{security:Security}) {
             <div className='flex w-fit text-nowrap px-2'>
             ${security.priceHistory[security.priceHistory.length-1].toFixed(2)}
             </div>
-            <div className='flex w-1/2 h-full items-center'>
-                ${(security.priceHistory[security.priceHistory.length-1]-security.priceHistory[security.priceHistory.length-2]).toFixed(2)}
-                <Image src={security.priceHistory[security.priceHistory.length-1]-security.priceHistory[security.priceHistory.length-2] > 0 ? "/security_up.svg" : "/security_down.svg"} alt="pm" width={30} height={10}/>
-
+            <div className={`flex w-1/2 h-full items-center ${changeSinceLastMonth >0?"text-green-700":"text-red-700"}`}>
+                ${changeSinceLastMonth.toFixed(2)}
+                <Image src={changeSinceLastMonth > 0 ? "/security_up.svg" : "/security_down.svg"} alt="pm" width={30} height={10}/>
+                ({percentageChange.toFixed(2)}%)
             </div>
             <Link className='bg-blue-500 text-nowrap hover:bg-blue-700 text-white px-2 w-fit rounded-lg h-10 justify-center items-center flex' href={"/securities/"+security.ticker}>More Details</Link>
         </div>
