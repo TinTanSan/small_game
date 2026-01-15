2e68b968b0f0d6421b056b64bc9068ce8f846165
TinTanSan <tirthvpatel2003@gmail.com>
2026-01-16 10:39:16 +1100

    completed initial implementation of security type navbar

--- src/app/securities/page.tsx
+++ src/app/securities/page.tsx
@@ -24,33 +24,34 @@ export default function Security() {
       setLoading(true)
       setUpLocalStorage();
       const securities = importSecurities();
-      setSecuritiesToShow(securities.filter(x=>selectedSecurityType?x.securityType==='Commodity' : x.securityType==='Equity'))
+      
       setMonth(getMonth())
       setLoading(false)
 
     },[])
-
+    useEffect(()=>{
+      const securities = importSecurities();
+      setSecuritiesToShow(securities.filter(x=>selectedSecurityType?x.securityType==='Commodity' : x.securityType==='Equity'))
+    },[selectedSecurityType])
     return (
       loading===false ?
         <div className='flex flex-col w-screen h-screen overflow-hidden items-center p-5 gap-2'>
-          <div id='security-type-nav' className='flex w-full h-12 rounded-lg px-2 bg-zinc-50 border-zinc-400 border-2'>
-            <div className='flex flex-col items-center'>
-              <button className={`flex flex-col w-fit `}>
-                <div className='flex w-50 text-lg justify-center items-center h-10'>Commodities</div>
-              </button>
-            </div>
-          </div>
-          {/* commodities */}
-          
-          <div className='flex flex-col border-2 w-full h-full items-center border-zinc-300 rounded-lg shadow-2xl'>
-              
-              <div className='flex flex-col w-full h-full gap-5 p-2'>
+            <div className='flex flex-col border-2 w-full h-full items-center border-zinc-300 rounded-lg shadow-2xl'>
+              <div id='security-type-nav' className='flex w-full h-12 rounded-lg p-1 px-2 gap-2'>
+                <button className={`flex flex-col w-fit rounded-md ${selectedSecurityType? "bg-blue-500 text-white" : "bg-base-300"} `} onClick={()=>{setSelectedSecurityType(true)}}>
+                  <div className='flex w-50 text-lg justify-center items-center h-10'>Commodities</div>
+                </button>
+                <button className={`flex flex-col w-fit rounded-md ${!selectedSecurityType? "bg-blue-500 text-white" : "bg-zinc-200"} `} onClick={()=>{setSelectedSecurityType(false)}}>
+                  <div className='flex w-50 text-lg justify-center items-center h-10'>Shares</div>
+                </button>
+              </div>
+              {/* minigraphs  */}
+              <div className='flex flex-col w-full h-full p-2 overflow-y-auto bg-base-200'>
+                <div className='flex flex-col w-full h-fit gap-5'>
                 {securitiesToShow.map((security)=><MiniGraph key={security.ticker} security={security} />)}
+                </div>
               </div>
           </div>
-          {/* equities */}
-
-          {/*  */}
         </div>
       :
         <div className='flex flex-col w-screen h-screen justify-center items-center relative'>
