6849775cad7d70f6fa31762ab52ca5ccd3729981
TinTanSan <tirthvpatel2003@gmail.com>
2026-01-16 09:34:32 +1100

    setting update to true when either month or balance changes

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { GameStateContext } from '../contexts/gameStateContext'
 import { updateGameState } from '@/utils/commons';
 import { PageContext } from '../contexts/pageContext';
@@ -12,14 +12,17 @@ export default function Navbar() {
       gameContext.setGameState(updateGameState(gameContext.gameState, 100))
       setUpdate(!update)
     }
-    
   }
+  useEffect(()=>{
+    setUpdate(true);
+    
+  },[gameContext?.gameState?.month, gameContext?.gameState?.balance])
   return (
-    <nav className='flex w-full h-14 items-center px-5 justify-between'>
+    <nav className='flex w-full h-14 items-center px-1 justify-between shadow-lg border-slate-400 border-2 rounded-lg'>
         <div className="text-xl flex w-1/2">${gameContext?.gameState?.balance.toFixed(2)}</div>
         <div className='flex w-1/2 text-lg'>Month: {gameContext?.gameState?.month}</div>
         <div className='flex w-full text-2xl'>{pageContext?.page || "Home"}</div>
-        <button onClick={()=>{handleNextMonth()}} className='flex bg-secondary text-secondary-content hover:bg-secondary-darken transition-colors duration-700 items-center justify-center w-fit flex-nowrap text-nowrap px-2 h-8 rounded-lg '>Next Month</button>
+        <button onClick={()=>{handleNextMonth()}} className='flex bg-secondary text-secondary-content hover:bg-secondary-darken transition-colors duration-700 items-center justify-center w-fit flex-nowrap text-nowrap px-2 h-4/5 rounded-lg '>Next Month</button>
     </nav>
   )
 }
